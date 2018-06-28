04a2856adf739c33bf7ea64199c26a31655865c3
GWC <gwc@macbook-pro-2.local>
2018-06-28 10:43:20 -0700

    app design!

--- finalProject/ViewController.swift
+++ finalProject/ViewController.swift
@@ -7,14 +7,13 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    override func viewDidLoad(){
+    super.viewDidLoad()
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
